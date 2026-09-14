2663af9951cf0a15e4b9812f3f01f64baee7be33
sammyschom <samschomberg999@gmail.com>
2026-09-14 15:08:42 +0100

    early pipeline for statsbomb open data

--- source-code/football_data/pipeline.py
+++ source-code/football_data/pipeline.py
@@ -1,5 +1,21 @@
+import requests
+
+
+COMPETITIONS_URL = (
+    "https://raw.githubusercontent.com/hudl/open-data/master/"
+    "data/competitions.json"
+)
+
 def main():
-    print("Football data pipeline starting...")
+    response = requests.get(COMPETITIONS_URL)
+
+    response.raise_for_status()
+
+    with open("data/raw/competitions.json", "wb") as file:
+        file.write(response.content)
+
+    print("Downloaded competitions.json")
+
 
 if __name__ == "__main__":
     main()
